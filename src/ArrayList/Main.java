fd72b07569a6ad9b7b3110ec0d60aadf41bd0704
duvan1026 <duvancastro1026@gmail.com>
2023-03-21 13:26:48 -0500

    implement create Arraylist and method .add in ArrayList

--- src/ArrayList/Main.java
+++ src/ArrayList/Main.java
@@ -1,8 +1,16 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args){
+
+        ArrayList<String> lista = new ArrayList<String>();
+
+        lista.add("ElementoA");
+        lista.add("ElementoB");
+        lista.add("ElementoC");
 
 
 
