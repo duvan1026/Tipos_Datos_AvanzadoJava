1a674f792f6cc8c7cbe5ec5b10957b9c3975fb90
duvan1026 <duvancastro1026@gmail.com>
2023-03-21 11:34:46 -0500

    implement method .add in vector

--- src/Vector/Main.java
+++ src/Vector/Main.java
@@ -1,10 +1,20 @@
 package Vector;
 
-public class Main {
+import java.util.Vector;
 
+public class Main {
+    /**
+     * Vectores, No es necesario definir un tamaño inicial o final,
+     * se ajustan automaticamente al tamaño de los datos.
+     * @param args
+     */
     public static void main(String[] args){
 
-
+        Vector<Integer> vector = new Vector<>();
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        System.out.println("Datos del vector: " + vector);
 
     }
 
