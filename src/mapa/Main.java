5f393ced9d01d39f11f0a925d1e02e67ea20fe9c
duvan1026 <duvancastro1026@gmail.com>
2023-03-21 14:28:22 -0500

    implement method remove() in HashMap

--- src/mapa/Main.java
+++ src/mapa/Main.java
@@ -14,13 +14,16 @@ public class Main {
         mapa.put("clave2",20);
         mapa.put("clave3",30);
 
-
+        mapa.remove("clave2"); // elimina la clave
         System.out.println(mapa);
+
+
+        /*
         System.out.println(mapa.get("clave1")); // Imprimimos el valor correspondiente a la clave setada
 
         //mapa.put("clave3", 90); // reemplaza  el valor correspondiente a la clave
         mapa.replace("clave4",190);// sino existe la clave la ignora
-
+        */
     }
 
 }
