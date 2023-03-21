e3e9dd9f13f4c96851a2ffe5a08041506538571b
Duvan Castro <duvancastro1026@gmail.com>
2023-03-21 05:44:49 -0500

    declare array and set values at each position

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args){
 
+        int arrayUno[] = new int[5]; // indicamos el limite de elementos
+        arrayUno[0] = 1;
+        arrayUno[1] = 2;
+        arrayUno[2] = 3;
+        arrayUno[3] = 4;
+        arrayUno[4] = 5;
+
+        for(int i: arrayUno){
+            System.out.println(i);
+        }
 
 
     }
