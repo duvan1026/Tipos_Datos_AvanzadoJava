fef4908988f8b843aad80210d9cf9c51709dff72
duvan1026 <duvancastro1026@gmail.com>
2023-03-21 12:18:05 -0500

    traverse vector and implement method .get()

--- src/Vector/Main.java
+++ src/Vector/Main.java
@@ -15,8 +15,22 @@ public class Main {
         vector.add(1);
         vector.add(2);
         vector.add(3);
+        vector.add(4);
 
-        /*
+        for(int i: vector){
+            System.out.println("Valor actual del vector: " + i);
+        }
+
+        for( int i = 0; i < vector.size(); i++){
+            if( i % 2 == 0 ){
+                vector.remove(i);
+            }else{
+                System.out.println("valores es: " + vector.get(i));
+            }
+
+        }
+
+        /* // Obtener el tamaño del vector y su capacidad
         System.out.println("Datos del vector: " + vector);
 
         vector.remove(2);// se setea la poscion del vector que se desea eliminar
@@ -25,6 +39,7 @@ public class Main {
         System.out.println("vector tamaño: " + vector.size() + " y capacidad: " + vector.capacity());
         */
 
+        /* // Comparar un vector con otro
         Vector<Integer> vector2 = new Vector<>();
         vector2.add(1);
         vector2.add(2);
@@ -32,7 +47,7 @@ public class Main {
 
         boolean resultado = vector.equals(vector2);
         System.out.println("Son iguales: " + resultado);
-
+        */
 
 
     }
