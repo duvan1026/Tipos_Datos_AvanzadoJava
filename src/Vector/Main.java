82ce93192e1dd2dcf433639b55e1ede866c33cde
duvan1026 <duvancastro1026@gmail.com>
2023-03-21 11:51:02 -0500

    implement method .size and .capacity

--- src/Vector/Main.java
+++ src/Vector/Main.java
@@ -6,18 +6,24 @@ public class Main {
     /**
      * Vectores, No es necesario definir un tamaño inicial o final,
      * se ajustan automaticamente al tamaño de los datos.
+     * Son dinamicos
      * @param args
      */
     public static void main(String[] args){
 
-        Vector<Integer> vector = new Vector<>();
+        Vector<Integer> vector = new Vector<>(1, 15); // capacidad inicial, incremento
         vector.add(1);
         vector.add(2);
         vector.add(3);
+
         System.out.println("Datos del vector: " + vector);
 
-        vector.remove(2);
+        vector.remove(2);// se setea la poscion del vector que se desea eliminar
         System.out.println("Datos del vector: " + vector);
+
+        System.out.println("vector tamaño: " + vector.size() + " y capacidad: " + vector.capacity());
+
+
 
     }
 
