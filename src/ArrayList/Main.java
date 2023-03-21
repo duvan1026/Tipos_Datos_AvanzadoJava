d965fca6a952072b75ae1ffba9456875dd82258f
duvan1026 <duvancastro1026@gmail.com>
2023-03-21 13:31:40 -0500

    implements .remove() in ArrayList

--- src/ArrayList/Main.java
+++ src/ArrayList/Main.java
@@ -6,11 +6,16 @@ public class Main {
 
     public static void main(String[] args){
 
+        // CapacidadArraylist = capacidadArrayList + (capacidadarrayList * 0.5)
         ArrayList<String> lista = new ArrayList<String>();
+
 
         lista.add("ElementoA");
         lista.add("ElementoB");
         lista.add("ElementoC");
+
+        lista.remove("ElementoB");
+        System.out.println("Contenido: " + lista);
 
 
 
