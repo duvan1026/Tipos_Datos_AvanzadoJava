dfc0338133321946b729a4bc6bd293a37729fb79
Duvan Castro <duvancastro1026@gmail.com>
2023-03-21 04:56:30 -0500

    file create

---
+++ src/String/Main.java
@@ -0,0 +1,15 @@
+package String;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        String cadena = "Mensaje de texto";
+
+        int cadenaLen = cadena.length(); // determinar el tamaño de la cadena
+        System.out.println("La longitud de mi cadena es: " + cadenaLen);
+
+
+
+    }
+}
