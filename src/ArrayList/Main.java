da1a64bcf66e6d2739034216935bcaba37362219
duvan1026 <duvancastro1026@gmail.com>
2023-03-21 13:43:16 -0500

    save ArrayList in array in java

--- src/ArrayList/Main.java
+++ src/ArrayList/Main.java
@@ -14,8 +14,32 @@ public class Main {
         lista.add("ElementoB");
         lista.add("ElementoC");
 
+        String array[] = new String[lista.size()];// setea el tamaño del array al tamaño del arraylist
+
+        for(int i = 0; i < lista.size(); i++){
+            array[i] = lista.get(i);
+        }
+
+        for(String element : array){
+            System.out.println(element);
+        }
+
+
+        /*
         lista.remove("ElementoB");
+        //lista.remove(2); // se puede eliminar tambien en posicion
         System.out.println("Contenido: " + lista);
+        */
+
+        /*  // recorrer los ArrayList
+        for(String nombre: lista){
+            System.out.println(nombre);
+        }
+
+        for(int i = 0; i < lista.size(); i++){
+            System.out.println(lista.get(i));
+        }
+        */
 
 
 
