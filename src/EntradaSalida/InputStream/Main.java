1dd8fd5109c4c862f2a34614561c32c71e54aaa5
duvan1026 <duvancastro1026@gmail.com>
2023-03-22 14:28:44 -0500

    Reading big file in Java

--- src/EntradaSalida/InputStream/Main.java
+++ src/EntradaSalida/InputStream/Main.java
@@ -9,12 +9,20 @@ public class Main {
 
     public static void main(String[] args){
         try{
-            InputStream fichero = new FileInputStream("/etc/password"); // Lee desde fichero
+            // Se coloca la ruta donde se neucentra alojado el fichero
+            InputStream fichero = new FileInputStream("J:\\Poryects_Duvan\\Spring\\Tipos_Datos_AvanzadoJava\\file.txt"); // Lee desde fichero
 
-            byte[] datos = fichero.readAllBytes();
+            // Se leeel fichero completo, pero no debe ser muy grande
+//            byte[] datos = fichero.readAllBytes();// se lee el fichero pero no debe ser muy grande
+//
+//            for(byte dato: datos){// Leer un fichero
+//                System.out.println((char) dato);
+//            }
 
-            for(byte dato: datos){// Leer un fichero
-                System.out.println((char) dato);
+            int dato = fichero.read();
+            while (dato != -1){// cuando sea igual a -1 no tiene mas datos que leer.
+                System.out.println(dato);
+                dato = fichero.read();
             }
 
         } catch ( FileNotFoundException e ){
