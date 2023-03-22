7cd00f4843f032cf769c7bde7f8e971b9c61e4dd
duvan1026 <duvancastro1026@gmail.com>
2023-03-22 14:39:36 -0500

    reading file fast in Java

--- src/EntradaSalida/InputStream/Main.java
+++ src/EntradaSalida/InputStream/Main.java
@@ -8,22 +8,36 @@ import java.io.InputStream;
 public class Main {
 
     public static void main(String[] args){
+
+        String urlFile = "J:\\Poryects_Duvan\\Spring\\Tipos_Datos_AvanzadoJava\\file.txt";
+
         try{
             // Se coloca la ruta donde se neucentra alojado el fichero
-            InputStream fichero = new FileInputStream("J:\\Poryects_Duvan\\Spring\\Tipos_Datos_AvanzadoJava\\file.txt"); // Lee desde fichero
+            InputStream fichero = new FileInputStream(urlFile); // Lee desde fichero
 
-            // Se leeel fichero completo, pero no debe ser muy grande
+            // Se lee el fichero completo, pero no debe ser muy grande
 //            byte[] datos = fichero.readAllBytes();// se lee el fichero pero no debe ser muy grande
 //
 //            for(byte dato: datos){// Leer un fichero
 //                System.out.println((char) dato);
 //            }
 
-            int dato = fichero.read();
+            // Se se lee el fichero bit a bit( se lee para ficheros grandes )
+//            int dato = fichero.read();
+//            while (dato != -1){// cuando sea igual a -1 no tiene mas datos que leer.
+//                System.out.println((char) dato);
+//                dato = fichero.read();
+//            }
+
+
+            byte datos[] = new byte[5]; // Lee en 5 en 5;
+            int dato = fichero.read(datos);
             while (dato != -1){// cuando sea igual a -1 no tiene mas datos que leer.
-                System.out.println(dato);
-                dato = fichero.read();
+                System.out.println(((char) dato));
+                dato = fichero.read(datos);
             }
+
+
 
         } catch ( FileNotFoundException e ){
             System.out.println("Error en la lectura del fichero: " + e.getClass());
