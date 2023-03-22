df42b70791069b21bddeaecf0dd84e3a4b831ade
duvan1026 <duvancastro1026@gmail.com>
2023-03-22 11:46:03 -0500

    reading file fichero and exception handling

--- src/EntradaSalida/InputStream/Main.java
+++ src/EntradaSalida/InputStream/Main.java
@@ -1,10 +1,17 @@
 package EntradaSalida.InputStream;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class Main {
 
     public static void main(String[] args){
-
-
+        try{
+            InputStream fichero = new FileInputStream("/etc/password"); // Lee desde fichero
+        } catch ( FileNotFoundException e ){
+            System.out.println("Error en la lectura del fichero");
+        }
 
 
     }
