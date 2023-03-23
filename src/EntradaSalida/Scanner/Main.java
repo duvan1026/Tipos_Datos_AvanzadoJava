629529a42583721027a8741b3707bda7baecffc2
duvan1026 <duvancastro1026@gmail.com>
2023-03-23 13:19:25 -0500

    implement method .reset() in Scanner

--- src/EntradaSalida/Scanner/Main.java
+++ src/EntradaSalida/Scanner/Main.java
@@ -1,6 +1,7 @@
 package EntradaSalida.Scanner;
 
 import java.io.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -12,10 +13,33 @@ public class Main {
 
         try{
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Ingresa un numero: ");
-            int a = scanner.nextInt();// Bloquea la ejecución del programa hasta que se ingrese un valor por consola.
-            int b = scanner.nextInt();// Bloquea la ejecución del programa hasta que se ingrese un valor por consola.
-            System.out.println("Enteros son: " + a + " " + b);
+
+            boolean ok = false;
+
+            do{
+                System.out.println("Ingrese dos numeros: ");
+                scanner.reset();
+                try{
+                    int a = scanner.nextInt();
+                    int b = scanner.nextInt();
+                    ok = true;
+                }catch ( InputMismatchException e) {
+                    System.out.println("Numeros invalidos");
+                }
+            } while (!ok);
+
+
+
+
+            // Se coloca la ruta donde se neucentra alojado el fichero
+            InputStream fichero = new FileInputStream(urlFile); // Lee desde fichero
+            BufferedInputStream ficherBuffer = new BufferedInputStream(fichero);// Accedemos lo que esta en memoria
+
+            int dato = fichero.read();
+            while (dato != -1){// cuando sea igual a -1 no tiene mas datos que leer.
+                System.out.println(((char) dato));
+                dato = ficherBuffer.read();
+            }
 
         } catch ( FileNotFoundException e ){
             System.out.println("Error en la lectura del fichero: " + e.getClass());
