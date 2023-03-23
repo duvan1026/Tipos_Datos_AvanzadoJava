e09d9adcf8c34539de8bd8a7b22d399fe664106b
duvan1026 <duvancastro1026@gmail.com>
2023-03-23 13:40:35 -0500

    copi file in Java

--- src/EntradaSalida/PrintStream/Main.java
+++ src/EntradaSalida/PrintStream/Main.java
@@ -1,4 +1,29 @@
 package EntradaSalida.PrintStream;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 public class Main {
+
+    public static void main(String[] args){
+
+        String urlFile = "J:\\Poryects_Duvan\\Spring\\Tipos_Datos_AvanzadoJava\\file.txt";
+
+        try{
+            InputStream in = new FileInputStream(urlFile);
+            byte[] datos = in.readAllBytes();
+
+
+            PrintStream out = new PrintStream("copia.txt");
+            out.write(datos);
+
+        }catch(Exception e){
+            System.out.println("Exception:" + e.getMessage());
+        }
+
+
+
+    }
+
 }
