fd1a09e891130e72f340af5b722cf3bea76d3c00
duvan1026 <duvancastro1026@gmail.com>
2023-03-23 13:46:50 -0500

    update file

--- src/EntradaSalida/PrintStream/Main.java
+++ src/EntradaSalida/PrintStream/Main.java
@@ -8,16 +8,16 @@ public class Main {
 
     public static void main(String[] args){
 
-        String urlFile = "J:\\Poryects_Duvan\\Spring\\Tipos_Datos_AvanzadoJava\\file.txt";
+        String urlFile = "J:\\Poryects_Duvan\\Spring\\Tipos_Datos_AvanzadoJava\\file.txt"; // fichero origen
 
         try{
             InputStream in = new FileInputStream(urlFile);
             byte[] datos = in.readAllBytes();
-            in.close();
+            in.close();// cierra el fichero
 
-            PrintStream out = new PrintStream("copia.txt");
+            PrintStream out = new PrintStream("copia.txt");// fichero destino
             out.write(datos);
-            out.close();
+            out.close();// cierra el fichero
 
         }catch(Exception e){
             System.out.println("Exception:" + e.getMessage());
