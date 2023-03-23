38c55b483dce802d4952db5222088953b3ee62d8
duvan1026 <duvancastro1026@gmail.com>
2023-03-23 13:22:09 -0500

    solution problems

--- src/EntradaSalida/Scanner/Main.java
+++ src/EntradaSalida/Scanner/Main.java
@@ -12,14 +12,16 @@ public class Main {
 
 
         try{
-            Scanner scanner = new Scanner(System.in);
+
 
             boolean ok = false;
 
             do{
+                Scanner scanner = new Scanner(System.in);
                 System.out.println("Ingrese dos numeros: ");
-                scanner.reset();
+
                 try{
+
                     int a = scanner.nextInt();
                     int b = scanner.nextInt();
                     ok = true;
